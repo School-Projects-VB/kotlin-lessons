809c907e7ac75c37434bfdb3d750530d0a7015f6
VBetsch <vic.betsch@gmail.com>
2022-10-24 17:49:14 +0200

    :sparkles: TP Data class

--- app/src/main/java/com/example/kotlintest/beans.kt
+++ app/src/main/java/com/example/kotlintest/beans.kt
@@ -29,6 +29,27 @@ fun main() {
     println("${plane.nom} : ${plane.id}")
     plane.nom = "bob"
     println("${plane.nom} : ${plane.id}")
+
+    val city = TownBean("Toulouse", "31000")
+    city.country = "toto"
+    val city2 = TownBean("Toulouse", "31000")
+    city2.country = "toto2"
+
+    val dataCity = DataTownBean("Toulouse", "31000")
+    dataCity.country = "toto"
+    val dataCity2 = DataTownBean("Toulouse", "31000")
+    dataCity2.country = "toto2"
+
+    println("city=$city") //city=TownBean@123245
+    println("dataCity=$dataCity")//dataCity=DataTownBean(city=Toulouse, cp=31000)
+
+    println("== : ${city == city2}") //== : false
+    println("equals : ${city.equals(city2)}")// equals : false
+    println("=== : ${city === city2}") //=== : false
+
+    println("data == : ${dataCity == dataCity2}") // data == : true
+    println("data equals : ${dataCity.equals(dataCity2)}")//data equals : true
+    println("data === : ${dataCity === dataCity2}") //data === : false
 }
 
 data class CarBean(var marque: String, var model: String, var couleur: String)
@@ -76,4 +97,11 @@ data class PlaneBean(val name: String) {
        field = value
        id = field.hashCode()
     }
+}
+
+class TownBean(val city: String, val cp: String) {
+    var country: String? = null
+}
+data class DataTownBean(val city: String, val cp: String) {
+    var country: String? = null
 }
