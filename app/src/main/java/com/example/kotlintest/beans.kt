953679d3beeb076d73cf8a3b9d856ba7861baecd
VBetsch <vic.betsch@gmail.com>
2022-10-24 16:57:10 +0200

    :sparkles: TP Constructeur secondaire

--- app/src/main/java/com/example/kotlintest/beans.kt
+++ app/src/main/java/com/example/kotlintest/beans.kt
@@ -1,5 +1,7 @@
 package com.example.kotlintest
 
+import java.util.Random
+
 fun main() {
     val car1 = CarBean("Seat", "Model", "grise")
     val car2 = CarBean2("Seat", "Model", "grise")
@@ -10,6 +12,9 @@ fun main() {
     eleve.note = 12
 
     println("Eleve : nom=${eleve.nom}, note=${eleve.note}")
+
+    PrintRandomIntBean(10)
+    PrintRandomIntBean()
 }
 
 data class CarBean(var marque: String, var model: String, var couleur: String)
@@ -26,4 +31,20 @@ class CarBean2(marque: String, model: String, couleur: String) {
 
 class StudentBean(val nom: String) {
     var note: Int = 0
+}
+
+class PrintRandomIntBean(var max: Int = 100){
+    private val random: Random = Random()
+    init {
+        if (max == 100) {
+            printNumbers(4)
+        } else {
+            printNumbers(3)
+        }
+    }
+    private fun printNumbers(times: Int) {
+        for (i in 1..times) {
+            println(random.nextInt(max))
+        }
+    }
 }
