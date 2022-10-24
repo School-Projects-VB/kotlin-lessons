75f28af03d446b36b436f466c75419ed8090a7da
VBetsch <vic.betsch@gmail.com>
2022-10-24 16:01:36 +0200

    :bug: Fix private attributes

--- app/src/main/java/com/example/kotlintest/beans.kt
+++ app/src/main/java/com/example/kotlintest/beans.kt
@@ -3,9 +3,9 @@ package com.example.kotlintest
 data class CarBean(var marque: String, var model: String, var couleur: String)
 
 class CarBean2(marque: String, model: String, couleur: String) {
-    var marque: String? = marque
-    var model: String? = model
-    var couleur: String? = couleur
+    private var marque: String? = marque
+    private var model: String? = model
+    private var couleur: String? = couleur
 
     override fun toString(): String {
         return "CarBean2(marque=${this.marque}, model=${this.model}, couleur=${this.couleur})"
