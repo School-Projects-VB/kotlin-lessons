d5b2e99c22582df7085fc4a081bff7eeb8f4f5ed
VBetsch <vic.betsch@gmail.com>
2022-10-24 14:52:05 +0200

    :sparkles: Add default values

--- app/src/main/java/com/example/kotlintest/exoKotlin.kt
+++ app/src/main/java/com/example/kotlintest/exoKotlin.kt
@@ -1,7 +1,8 @@
 package com.example.kotlintest
 
 fun main() {
-    println(boulangerie(3, 5, 2))
+    println(boulangerie(2))
+    println(boulangerie(baguettes = 2, sandwichs = 3))
 }
 
 // min
@@ -16,6 +17,6 @@ fun myPrint(str: String): String = "#$str#"
 // Quel est le type de retour de myPrint ?
 // String
 
-fun boulangerie(croissants: Int, baguettes: Int, sandwichs: Int): Double {
+fun boulangerie(croissants: Int = 0, baguettes: Int = 0, sandwichs: Int = 0): Double {
     return (croissants*PRIX_CROISSANT + baguettes*PRIX_BAGUETTE + sandwichs*PRIX_SANDWICH)
 }
