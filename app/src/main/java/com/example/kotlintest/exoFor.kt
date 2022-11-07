e86fa7717c4f1592d6dc9bf1d24e01fc84762c1f
VBetsch <vic.betsch@gmail.com>
2022-11-07 15:32:37 +0100

    :sparkles: TP For question 4

--- app/src/main/java/com/example/kotlintest/exoFor.kt
+++ app/src/main/java/com/example/kotlintest/exoFor.kt
@@ -1,10 +1,10 @@
 package com.example.kotlintest
 
 fun main() {
-    println(reverseSentence("ceci est un test"))
+    println(countCaps("CeCi eSt Un TeSt"))
 }
 
-// Return string without 'e' letter
+// 1 - Return string without 'e' letter
 fun getWithoutE(sentence :String) :String {
     var result = ""
     for (letter in sentence) {
@@ -15,7 +15,7 @@ fun getWithoutE(sentence :String) :String {
     return result
 }
 
-// Return the count of 'a' letter
+// 2 - Return the count of 'a' letter
 fun countOfA(sentence: String) :Int {
     var counter = 0
     for (letter in sentence) {
@@ -26,11 +26,22 @@ fun countOfA(sentence: String) :Int {
     return counter
 }
 
-// Return reverse string
+// 3 - Return reverse string
 fun reverseSentence(sentence :String) :String{
     var result = ""
     for (i in sentence.length - 1 downTo 0) {
         result += sentence[i]
     }
     return result
+}
+
+// 4 - Return count of caps
+fun countCaps(sentence :String) :Int{
+    var counter = 0
+    for (letter in sentence) {
+        if (letter in 'A'..'Z') {
+            counter++
+        }
+    }
+    return counter
 }
