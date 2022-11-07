72945b889b8eff509471a76bbdc710cc946f6e7f
VBetsch <vic.betsch@gmail.com>
2022-11-07 12:43:19 +0100

    :bug: Fix val warnings

--- app/src/main/java/com/example/kotlintest/miniProjet.kt
+++ app/src/main/java/com/example/kotlintest/miniProjet.kt
@@ -3,7 +3,7 @@ package com.example.kotlintest
 fun main() {
     print("Enter city: ")
     val city = readLine()!!
-    var res = RequestUtils.loadWeather(city)
-    var result = "Il fait " + res.temperature.temp + "° à " + city + " avec un vent de " + res.wind.speed + " km/h"
+    val res = RequestUtils.loadWeather(city)
+    val result = "Il fait " + res.temperature.temp + "° à " + city + " avec un vent de " + res.wind.speed + " km/h"
     println(result)
 }
