797874353feb3ef5e7d800ab8559c92816247571
VBetsch <vic.betsch@gmail.com>
2022-11-07 12:44:42 +0100

    :bug: Fix val warnings

--- app/src/main/java/com/example/kotlintest/RequestUtils.kt
+++ app/src/main/java/com/example/kotlintest/RequestUtils.kt
@@ -5,7 +5,7 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 
 fun main() {
-    var weather = RequestUtils.loadWeather("Bordeaux")
+    val weather = RequestUtils.loadWeather("Bordeaux")
     println(weather)
 }
 
@@ -14,10 +14,9 @@ object RequestUtils {
     var gson = Gson()
 
     fun loadWeather(city: String): WeatherBean {
-        var url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=b80967f0a6bd10d23e44848547b26550&units=metric&lang=fr"
+        val url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=b80967f0a6bd10d23e44848547b26550&units=metric&lang=fr"
         val json: String = sendGet(url)
-        var data = gson.fromJson(json, WeatherBean::class.java)
-        return data
+        return gson.fromJson(json, WeatherBean::class.java)
     }
 
     fun sendGet(url: String): String {
