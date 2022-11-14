e810baa37521d89a0332ec304e7872bd74f39b0d
VBetsch <vic.betsch@gmail.com>
2022-11-14 17:14:36 +0100

    :sparkles: TP manipulation IHM 1

--- app/src/main/java/com/example/kotlintest/MainActivity.kt
+++ app/src/main/java/com/example/kotlintest/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.kotlintest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kotlintest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.btValidate.setOnClickListener {
+            binding.etName.hint = "clic sur valider"
+        }
     }
 }
