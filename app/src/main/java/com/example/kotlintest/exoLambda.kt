f389cf5438f66763ead4b6af9cad952037316c0c
VBetsch <vic.betsch@gmail.com>
2022-11-14 11:47:24 +0100

    :sparkles: TP lambda High Order Function

--- app/src/main/java/com/example/kotlintest/exoLambda.kt
+++ app/src/main/java/com/example/kotlintest/exoLambda.kt
@@ -1,7 +1,7 @@
 package com.example.kotlintest
 
 fun main() {
-    exo1()
+    exo2()
 }
 
 fun exo1() {
@@ -19,4 +19,20 @@ fun exo1() {
 
     val minToMinHour: (Int) -> Pair<Int, Int> = { it/60 to it %60 }
     println(minToMinHour(142))
+}
+
+fun exo2() {
+    val compareUsersByNote = { A: UserBean, B: UserBean -> if(A.note >= B.note) A else B }
+    val compareUsersByName = { A: UserBean, B: UserBean -> if(A.nom <= B.nom) A else B }
+
+    var User1 = UserBean("Albert", 12)
+    var User2 = UserBean("Michel", 16)
+    var User3 = UserBean("Jean", 14)
+
+    val u1 = UserBean("Bob", 19)
+    val u2 = UserBean("Toto", 45)
+    val u3 = UserBean("Charles", 26)
+
+    println(compareUsersByNote(User1, User2))
+    println(compareUsersByName(User1, User2))
 }
