a74681f283a70814f37ae7637cfdcb97dc481656
VBetsch <vic.betsch@gmail.com>
2022-11-14 17:40:31 +0100

    :sparkles: Added cancel button

--- app/src/main/java/com/example/kotlintest/MainActivity.kt
+++ app/src/main/java/com/example/kotlintest/MainActivity.kt
@@ -7,16 +7,20 @@ import com.example.kotlintest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         binding.btValidate.setOnClickListener(this)
+        binding.btCancel.setOnClickListener(this)
     }
 
     override fun onClick(view: View) {
-        binding.etName.hint = "clic sur valider"
+        when(view) {
+            binding.btValidate -> binding.etName.hint = "Validé"
+            binding.btCancel -> binding.etName.hint = "Annulé"
+        }
     }
 }
