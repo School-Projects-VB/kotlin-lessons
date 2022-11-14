30fc2121b60b713eac310a1d427bcc2fd33bbfbb
VBetsch <vic.betsch@gmail.com>
2022-11-14 11:18:16 +0100

    :bug: Correction

--- app/src/main/java/com/example/kotlintest/exoLambda.kt
+++ app/src/main/java/com/example/kotlintest/exoLambda.kt
@@ -5,18 +5,18 @@ fun main() {
 }
 
 fun exo1() {
-    val Lower: (String) -> Unit = { println(it.lowercase()) }
+    val Lower = { text:String -> println(text.lowercase()) }
     Lower("TeSt TEST")
 
     val Heure: (Int) -> Int = { it/60 }
     println(Heure(142))
 
-    val Max: (Int, Int) -> Int = { a: Int, b: Int -> if(a > b) a else b }
+    val Max = { a: Int, b: Int -> if(a > b) a else b }
     println(Max(3,5))
 
     val Reverse: (String) -> String = { it.reversed() }
     println(Reverse("toto test"))
 
-    val minToMinHour: (Int) -> Pair<Int, Int> = { Pair(it/60, it %60) }
+    val minToMinHour: (Int) -> Pair<Int, Int> = { it/60 to it %60 }
     println(minToMinHour(142))
 }
