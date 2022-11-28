0118481f2a551c712201a0639f39807ef35811da
VBetsch <vic.betsch@gmail.com>
2022-11-28 14:34:00 +0100

    :bug: Correction

--- app/src/main/java/com/example/kotlintest/MainActivity.kt
+++ app/src/main/java/com/example/kotlintest/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kotlintest
 
+import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
@@ -8,11 +9,22 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.DatePicker
+import android.widget.TimePicker
 import android.widget.Toast
 import com.example.kotlintest.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
 import java.util.Calendar
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+private const val MENU_ID_TIME_PICKER = 1
+private const val MENU_ID_DATE_PICKER = 2
+private const val MENU_ID_ALERT_DIALOG = 3
+
+@SuppressLint("SimpleDateFormat")
+val SDF = SimpleDateFormat("dd/MM/yy HH:mm")
+
+class MainActivity : AppCompatActivity(), View.OnClickListener, TimePickerDialog.OnTimeSetListener,
+    DatePickerDialog.OnDateSetListener {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private val calendar: Calendar = Calendar.getInstance()
@@ -35,24 +47,31 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menu.add(0, 1, 0, "TimePicker")
-        menu.add(0, 2, 0, "DatePicker")
-        menu.add(0, 3, 0, "AlertDialog")
+        menu.add(0, MENU_ID_TIME_PICKER, 0, "TimePicker")
+        menu.add(0, MENU_ID_DATE_PICKER, 0, "DatePicker")
+        menu.add(0, MENU_ID_ALERT_DIALOG, 0, "AlertDialog")
 
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val timePickerDialogListener: TimePickerDialog.OnTimeSetListener =
-            TimePickerDialog.OnTimeSetListener { context, hour, minute -> Toast.makeText(this, "La nouvelle heure est ${hour}:${minute}", Toast.LENGTH_LONG).show() }
-        val datePickerDialogListener: DatePickerDialog.OnDateSetListener =
-            DatePickerDialog.OnDateSetListener {context, year, month, day -> Toast.makeText(this, "La nouvelle date est ${day}/${month+1}/${year}", Toast.LENGTH_LONG).show()}
-        val alert = AlertDialog.Builder(this)
         when (item.itemId) {
-            1 -> TimePickerDialog(this, timePickerDialogListener, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true).show()
-            2 -> DatePickerDialog(this, datePickerDialogListener,calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH)).show()
-            3 -> alert.setTitle("Mon titre").setMessage("Afficher un toast").setPositiveButton("OK") {dialog, which -> Toast.makeText(this, "Ceci est un toast", Toast.LENGTH_SHORT).show()}.setIcon(R.mipmap.ic_launcher).show()
+            MENU_ID_TIME_PICKER -> TimePickerDialog(this, this, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true).show()
+            MENU_ID_DATE_PICKER -> DatePickerDialog(this, this, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show()
+            MENU_ID_ALERT_DIALOG -> AlertDialog.Builder(this).setTitle("Mon titre").setMessage("Afficher un toast").setPositiveButton("OK") {dialog, which -> Toast.makeText(this, "Ceci est un toast", Toast.LENGTH_SHORT).show()}.setIcon(R.mipmap.ic_launcher).show()
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay)
+        calendar.set(Calendar.MINUTE, minute)
+
+        Toast.makeText(this, SDF.format(calendar.time), Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        calendar.set(year, month, dayOfMonth)
+        Toast.makeText(this, SDF.format(calendar.time), Toast.LENGTH_SHORT).show()
     }
 }
