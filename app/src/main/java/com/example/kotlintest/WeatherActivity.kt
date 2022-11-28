79fd3a7c2fd5e2780809b61869b076dc0dde7e47
VBetsch <vic.betsch@gmail.com>
2022-11-28 16:25:12 +0100

    :sparkles: TP Thread Vrai requête

--- app/src/main/java/com/example/kotlintest/WeatherActivity.kt
+++ app/src/main/java/com/example/kotlintest/WeatherActivity.kt
@@ -1,14 +1,39 @@
 package com.example.kotlintest
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.kotlintest.databinding.ActivityWeatherBinding
+import com.example.kotlintest.exokotlin.RequestUtils
+import kotlin.concurrent.thread
 
-class WeatherActivity : AppCompatActivity() {
+class WeatherActivity : AppCompatActivity(), View.OnClickListener {
     private val binding by lazy { ActivityWeatherBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.btLoad.setOnClickListener(this)
+        binding.btAnnuler.setOnClickListener(this)
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onClick(view: View) {
+        val utils = RequestUtils
+
+        when(view) {
+            binding.btLoad -> {
+                binding.text.text = "Waiting..."
+                thread {
+                    var weather = utils.loadWeather("Bordeaux")
+                    runOnUiThread {
+                        binding.text.text = "Il fait ${weather.temperature.temp}° à ${weather.name} avec un vent de ${weather.wind.speed} km/h"
+                    }
+                }
+            }
+            binding.btAnnuler -> {binding.text.text = getText(R.string.tv)}
+        }
     }
 }
