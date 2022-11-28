9d2f606bf7a4dcb473a0dd3aa8f33d8c2989fd0e
VBetsch <vic.betsch@gmail.com>
2022-11-28 14:14:51 +0100

    :sparkles: TP Menus et dialogues Part 3

--- app/src/main/java/com/example/kotlintest/MainActivity.kt
+++ app/src/main/java/com/example/kotlintest/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kotlintest
 
+import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -34,8 +35,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menu.add(0, 25, 0, "foo")
-        menu.add(0, 26, 0, "bar")
+        menu.add(0, 1, 0, "foo")
+        menu.add(0, 2, 0, "bar")
+        menu.add(0, 3, 0, "baz")
 
         return super.onCreateOptionsMenu(menu)
     }
@@ -45,9 +47,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             TimePickerDialog.OnTimeSetListener { context, hour, minute -> Toast.makeText(this, "La nouvelle heure est ${hour}:${minute}", Toast.LENGTH_LONG).show() }
         val datePickerDialogListener: DatePickerDialog.OnDateSetListener =
             DatePickerDialog.OnDateSetListener {context, year, month, day -> Toast.makeText(this, "La nouvelle date est ${day}/${month+1}/${year}", Toast.LENGTH_LONG).show()}
+        val alert = AlertDialog.Builder(this)
         when (item.itemId) {
-            25 -> TimePickerDialog(this, timePickerDialogListener, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true).show()
-            26 -> DatePickerDialog(this, datePickerDialogListener,calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH)).show()
+            1 -> TimePickerDialog(this, timePickerDialogListener, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true).show()
+            2 -> DatePickerDialog(this, datePickerDialogListener,calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH)).show()
+            3 -> alert.setTitle("Mon titre").setMessage("Afficher un toast").setPositiveButton("OK") {dialog, which -> Toast.makeText(this, "Ceci est un toast", Toast.LENGTH_SHORT).show()}.setIcon(R.mipmap.ic_launcher).show()
         }
         return super.onOptionsItemSelected(item)
     }
